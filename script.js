88232427d5141f4b7a1f8f23a089a9224bc4839f
Mandeep Shergill <mandeep@shergillgames.com>
2013-02-14 18:10:03 -0500

    Added "view last image" button

--- script.js
+++ script.js
@@ -1,28 +1,44 @@
-// I don't know what I'm doing! Hopefully I'll learn. Or not. "Failure is always an option."
+// I don't know what I'm doing!
 var xhr = new XMLHttpRequest();
-var imgSrc;
+var imgSrc = 0;
+var lastImg = 0;
+
+function loadImgur() {
+// Got help from https://github.com/crsrusl/imgurGallery/blob/master/app.js
+    $.ajax({
+        dataType: "json",
+        mimeType: "textPlain", // Firefox 19 not working without this
+        type: "GET",
+        crossDomain:true,
+        url: "https://api.imgur.com/3/gallery/r/aww/top/0/all",
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader("Authorization", "Client-ID da42354d6ffb19a");
+        },
+        success: function(imgur) {
+            var limit = imgur.data.length;
+            var x = Math.floor(Math.random()*limit);    
+
+            if (imgSrc != 0) {lastImg = imgSrc;}
+
+            imgSrc = imgur.data[x].id;
+            imgSrc = "" + imgSrc;
+
+            $('#main').html('<img src="http://i.imgur.com/' + imgSrc + '.jpg"/>');
+        }
+    });
+}
 
 $(document).ready(function() {
+    loadImgur();
     $('#ImgurAPI').click(function() {
-// got help from https://github.com/crsrusl/imgurGallery/blob/master/app.js
-        $.ajax({
-            dataType: "json",
-            mimeType: "textPlain", // Firefox 19 not working without this
-            type: "GET",
-            crossDomain:true,
-            url: "https://api.imgur.com/3/gallery/r/aww/top/0/all",
-            beforeSend: function(xhr) {
-                xhr.setRequestHeader("Authorization", "Client-ID da42354d6ffb19a");
-            },
-            success: function(imgur) {
-                var limit = imgur.data.length;
-                var x = Math.floor(Math.random()*limit);    
-
-                imgSrc = imgur.data[x].id;
-                imgSrc = "" + imgSrc;
-
-                $('#main').html('<img src="http://i.imgur.com/' + imgSrc + '.jpg"/>');
-            }
-        });
+        loadImgur();
     });
+    
+    $('#LastImg').click(function() {
+        if (lastImg != 0) {
+            $('#main').html('<img src="http://i.imgur.com/' + lastImg + '.jpg"/>');
+            imgSrc = 0;
+        }
+    });
+    
 });
