d06a8b26d8a2a85a3dd773c629de38eb74e31f2a
Mandeep Shergill <mandeep@shergillgames.com>
2013-03-20 10:33:33 -0400

    Changed relative install path to absolute since workaround failed

--- script.js
+++ script.js
@@ -67,9 +67,9 @@ $(document).ready(function() {
 
     $('#B2G').click(function() {
         // relative path bug - https://bugzilla.mozilla.org/show_bug.cgi?id=745928
-        //navigator.mozApps.install('http://mandeeps.github.com/CheerUp/manifest.webapp').onsuccess = function() {
+        navigator.mozApps.install('http://mandeeps.github.com/CheerUp/manifest.webapp').onsuccess = function() {
         //navigator.mozApps.install(window.location.href.substring(0, window.location.href.lastIndexOf('/')) + "/manifest.webapp").onsuccess = function() {
-        navigator.mozApps.install(location.protocol + "//" + location.host + "/manifest.webapp").onsuccess = function() {
+        //navigator.mozApps.install(location.protocol + "//" + location.host + "/manifest.webapp").onsuccess = function() {
             $('#B2G').css('visibility', 'hidden');
         };
     });
