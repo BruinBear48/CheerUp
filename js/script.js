d5a43aa3ffd273121218ed3031b92d85f72761a4
Mandeep Shergill <mandeep@shergillgames.com>
2013-05-31 18:36:30 -0400

    Smaller images for appCache, Zepto instead of jQuery, better notifiers, offline viewing of downloaded images instead of only appCache ones

--- js/script.js
+++ js/script.js
@@ -4,16 +4,46 @@ var imgC = 0;
 var list = [];
 var canInstall = !!(navigator.mozApps && navigator.mozApps.install);
 var lowRes = false;
-var albumCount = 1; // 56 images per album
+var albumCount = 3; // "albumCount * imgPerAlbum pet photos ought to be enough for anybody!"
+var imgPerAlbum = 56; // Specify images per album, in case Imgur changes this
 var offlineList = ['img/offline1.jpg', 'img/offline2.jpg', 'img/offline3.jpg', 'img/offline4.jpg', 'img/offline5.jpg'];
+var preloaded = false;
+var installed = false;
 
-var dbCache = new IDBStore({
+/*var dbCache = new IDBStore({
 	storeName: 'cache',
 	dbVersion: 1,
 	keyPath: 'id',
 	autoIncrement: true,
 	onStoreReady: function(){console.log('indexedDB available');}
-});
+});*/
+
+// Switch from jQuery to Zepto, need CSS3 animations...
+$.fn.slideDown = function() {
+	var duration = 400;
+	this.css({		
+		visibility: 'hidden'
+	});
+	this.show();
+
+    var distance = 0 - this.height();
+    this.css({
+		top: distance, 
+		visibility: 'visible',
+    });
+
+    this.animate({
+		top: 0
+	}, duration);
+};
+
+$.fn.slideUp = function() {    
+    var duration = 400;
+    var distance = 0 - this.height();
+    this.animate({
+		top: distance
+    }, duration);
+};
 
 function preLoad() {
 	// preload images
@@ -36,6 +66,7 @@ function preLoad() {
 }
 
 function loadImgur() {
+	var albumsLoaded = 0;
 	for (var i = 0; i < albumCount; i++) {
         $.ajax({
             dataType: "json",
@@ -48,21 +79,19 @@ function loadImgur() {
             },
 
             success: function(imgur) {
+				albumsLoaded++;
                 for (var y = 0; y < imgur.data.length; y++) {
                     if (lowRes) {
                         list.push(imgur.data[y].link.replace(/.([^.]*)$/, 'l.$1'));
                     }
                     else {list.push(imgur.data[y].link);}
                 }
-                if (i == albumCount) {
+                if (albumsLoaded == albumCount) {
 					console.log('# images to show: ' + list.length)
 					preLoad().done(function() {
 						$('#notify').slideUp();
-						setControls();
 						console.log('preload completed');
-						//for (var i = 0; i < list.length; i++) {
-							//saveImage(preImg[i], list[i]);
-						//}
+						preloaded = true;
 					});
 				}
             },
@@ -85,16 +114,19 @@ function windowSize() {
 }
 
 function imgTransition() {
-    $('#photo').fadeOut(30);
+//ZEPTO    $('#photo').fadeOut(30);
     $('#photo').attr('src', curImg);
-    $('#photo').fadeIn(750);
+//ZEPTO    $('#photo').fadeIn(750);
 }
 
 function pickImage() {
-    var x = Math.floor(Math.random()*list.length);
-    lastImg = curImg;
-    if (list[x] != lastImg) {
-		curImg = list[x];
+    if (preloaded) {var listUsed = list;}
+	else {var listUsed = offlineList;}
+	
+	var x = Math.floor(Math.random()*listUsed.length);
+	lastImg = curImg;
+	if (listUsed[x] != lastImg) {
+		curImg = listUsed[x];
 		imgTransition();
 	}
 	else {pickImage();}
@@ -107,6 +139,7 @@ function setControls() {
 		request.onsuccess = function getSelfSuccess() {
 			if (request.result) {
 				// already installed as Firefox webapp, hide github
+				installed = true;
 				$('.github-ribbon').css('visibility', 'hidden');
 			}
 			else {
@@ -119,26 +152,20 @@ function setControls() {
 
 function offline() {
 	console.log('now offline!!!');
-	$('#notify').hide();
-	var offlineMessage = document.createElement('p');
-	var offlineText = document.createTextNode('This app needs internet access');
-	offlineMessage.appendChild(offlineText);
-	$('#main').prepend(offlineMessage);
+	$('#notify').html('Working offline...<br>Click to close');
+	$('#notify').slideDown();
+	$('#notify').click(function() {
+		$(this).slideUp();
+	});
+
 	$('.github-ribbon').css('visibility', 'hidden');
-	$('#controls').css('visibility', 'hidden');
-	var offImg = 0;	
-	timer = setInterval(function() {
-		if (offImg < 4) {offImg++;}
-		else {offImg = 0;}
-		curImg = offlineList[offImg];
-		imgTransition();
-	}, 3000);
 }
 
 $(document).ready(function() {
-    $('#notify').slideDown();
+    setControls();
     
     if (navigator.onLine) {
+		$('#notify').slideDown();
 		loadImgur();
     }
     else {offline();}
@@ -147,11 +174,14 @@ $(document).ready(function() {
 
 	window.addEventListener('online', function() {
 		console.log('now online');
-		if ($('p').length) {$('p').remove();}
-		$('.github-ribbon').css('visibility', 'visible');
-		$('#controls').css('visibility', 'visible');	
-		clearInterval(timer);
-		if (list.length < 10) {loadImgur();}
+		if (!installed) {$('.github-ribbon').css('visibility', 'visible');}
+		if (!preloaded) {
+			$('#notify').html('Click photos to see comments.<br>Loading...');
+			$('#notify').slideDown();
+		}
+		else {$('#notify').slideUp();}
+		
+		if (list.length < albumCount * imgPerAlbum) {loadImgur();}
 	});
     
     windowSize();
@@ -177,7 +207,7 @@ $(document).ready(function() {
     });
     
     $('#photo').click(function() {
-        if (navigator.onLine && curImg != 0) {
+        if (navigator.onLine && curImg != 0 && preloaded) {
             var imgPage = curImg.replace(/.([^.]*)$/, '');
             if (lowRes) {
                 imgPage = imgPage.replace(/.([^l]*)$/, '');
