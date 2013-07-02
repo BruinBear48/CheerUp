5899516318390df0b9d685b853b66abb2a2075ce
Mandeep Shergill <mandeep@shergillgames.com>
2013-07-02 16:02:58 -0400

    A few small attempts at improving performance following browserdiet.com

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
 (function() {
-  var curImg = 0;
-  var lastImg = 0;
-  var imgC = 0;
-  var list = [];
-  var lowRes = true;
+  var curImg = 0, lastImg = 0, imgC = 0, list = [], lowRes = true;
   var ALBUM_COUNT = 2; // ALBUM_COUNT * ALBUM_IMAGES pet photos should be enough for anybody!
   var ALBUM_IMAGES = 56; // Specify images per album, in case Imgur changes this
   var offlineList = ['img/offline1.jpg', 'img/offline2.jpg', 'img/offline3.jpg',
    'img/offline4.jpg', 'img/offline5.jpg'];
-  var preloaded = false;
-  var installed = false;
+  var preloaded = false, installed = false;
   //var url = "https://api.imgur.com/3/gallery/r/aww/top/all/"; // maybe let users select gallery at some point?
+
+  // Don't query the DOM for these elements more than once
+  var notifyElem = $('#notify');
+  var photoElem = $('#photo');
+  var githubElem = $('.github-ribbon');
+  var installElem = $('#B2G');
   
   var storage = window.localStorage;
     // Use localStorage to check if images previously downloaded into cache
@@ -48,17 +49,18 @@
   };
   
   // Preload images for smoother transitions, offline capability
-  function preLoad() {
-    var preImg = [], loaded = 0, count = list.length;
+  function preLoad(count) {
+    var preImg = [], loaded = 0;
     for (var i = 0; i < count; i++) {
       preImg[i] = new Image();
-      preImg[i].src = list[i];
-      preImg[i].onload = function() {
+      var selected = preImg[i];
+      selected.src = list[i];
+      selected.onload = function() {
         var i2 = preImg.indexOf(this);
         var imageBeingLoaded = list[i2];
         offlineList.push(imageBeingLoaded);
         loaded++;
-        if (loaded == list.length) {
+        if (loaded === count) {
           whenDone();
         }
       };
@@ -92,10 +94,11 @@
             }
             else {list.push(imgur.data[y].link);}
           }
-          if (albumsLoaded == ALBUM_COUNT) {
-            console.log('# images to show: ' + list.length)
-            preLoad().done(function() {
-              $('#notify').slideUp();
+          if (albumsLoaded === ALBUM_COUNT) {
+            var count = list.length;
+            console.log('# images to show: ' + count);
+            preLoad(count).done(function() {
+              notifyElem.slideUp();
               preloaded = true;
               offlineList.splice(0, 5);
               storage.localStoreList = JSON.stringify(offlineList);
@@ -122,15 +125,16 @@
   
   function imgTransition() {
     // animate new photo display
-    $('#photo').hide();
-    $('#photo').attr('src', curImg);
-    $('#photo').fadeIn(100);
+    photoElem.hide();
+    photoElem.attr({'width': curImg.width, 'height': curImg.height, 'src': curImg});
+    photoElem.fadeIn(200);
   }
   
   function pickImage() {
     // choose image to display, can't be same as one just shown
     var listUsed = offlineList;
-    var x = Math.floor(Math.random()*listUsed.length);
+    var size = listUsed.length
+    var x = Math.floor(Math.random()*size);
     lastImg = curImg;
     if (listUsed[x] != lastImg) {
       curImg = listUsed[x];
@@ -159,7 +163,6 @@
   }
   
   function setControls() {
-    $('#controls').css('visibility', 'visible');
     var canInstall = !!(navigator.mozApps && navigator.mozApps.install);
     if (canInstall) {
       var request = window.navigator.mozApps.getSelf();
@@ -167,11 +170,11 @@
         if (request.result) {
           // already installed as Firefox webapp, hide github link
           installed = true;
-          $('.github-ribbon').hide();
+          githubElem.hide();
         }
         else {
           // not installed so show install button
-          $('#B2G').css('display', 'inline');
+          installElem.css('display', 'inline');
         }
       };
     }
@@ -179,9 +182,9 @@
   
   function offline() {
     console.log('now offline!!!');
-    $('#notify').html('Working offline...<br>Click to close');
-    $('#notify').slideDown();
-    $('.github-ribbon').css('display', 'none');
+    notifyElem.html('Working offline...<br>Click to close');
+    notifyElem.slideDown();
+    githubElem.css('display', 'none');
     if (storage.localStoreList.length > ALBUM_IMAGES) {
       offlineList = JSON.parse(storage.localStoreList);
     }
@@ -189,12 +192,12 @@
   
   function online() {
     console.log('now online');
-    if (!installed) {$('.github-ribbon').css('display', 'block');}
+    if (!installed) {githubElem.css('display', 'block');}
     if (!preloaded) {
-      $('#notify').html('Click photos for comments once loaded.<br>Loading...');
-      $('#notify').slideDown();
+      notifyElem.html('Click photos for comments once loaded.<br>Loading...');
+      notifyElem.slideDown();
     }
-    else {$('#notify').slideUp();}
+    else {notifyElem.slideUp();}
   
     if (list.length < ALBUM_COUNT * ALBUM_IMAGES) {
       console.log('online, but not all images loaded so running loadImgur');
@@ -208,7 +211,7 @@
     base = base.replace('index.html', '');
     var mozillaInstallUrl = base + '/manifest.webapp';
     navigator.mozApps.install(mozillaInstallUrl).onsuccess = function() {
-      $('#B2G').css('display', 'none');
+      installElem.css('display', 'none');
     };
   }
   
@@ -216,39 +219,39 @@
     if (window.innerWidth > 1024 || window.innerHeight > 1024) {
       // only load higher res images if browser has > 1024px available
       if (firstRun) {lowRes = false;}
-      $('.github-ribbon').text("View code on Github");
+      githubElem.text("View code on Github");
     }
-    else {$('.github-ribbon').text("view code...");}
+    else {githubElem.text("view code...");}
   
-    if (preloaded) {$('#notify').slideUp();}
+    if (preloaded) {notifyElem.slideUp();}
   }
   
   $(document).ready(function() {
-    setControls();
-    windowSize(true);
-    window.onresize = function() {
-      windowSize(false);
-    }
-  
-    $('.github-ribbon').click(function() {
-      $(this).hide();
-    });
-    $('#notify').click(function() {
-      $(this).slideUp();
-    });
-  
     if (navigator.onLine) {
       console.log('first run online check...');
-      $('#notify').slideDown();
+      notifyElem.slideDown();
       loadImgur();
     }
     else {offline();}
   
     window.addEventListener('offline', offline);
     window.addEventListener('online', online);
+
+    setControls();
+    windowSize(true);
+    window.onresize = function() {
+      windowSize(false);
+    }
+  
+    githubElem.click(function() {
+      $(this).hide();
+    });
+    notifyElem.click(function() {
+      $(this).slideUp();
+    });
     $('#ImgurAPI').click(pickImage);
     $('#LastImg').click(viewLastImage);
-    $('#photo').click(visitImgur);
-    $('#B2G').click(installerFF);
+    photoElem.click(visitImgur);
+    installElem.click(installerFF);
   });
 })();  
